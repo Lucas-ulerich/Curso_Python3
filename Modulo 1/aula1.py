5166ad5572641578c7d6e7072ec080df09b39b10
Lucas-ulerich <lucas.ulerichsouza@gmail.com>
2024-04-20 10:16:33 -0300

    Adicionando aulas 1 e 2 do modulo 1

---
+++ Modulo 1/aula1.py
@@ -0,0 +1,14 @@
+# Isso é um comentário de código Python!!
+# Comentário de uma linha.
+# O interpretador ignora qualquer comentário, ele não será executado, serve para documentar seu código.
+
+print(123) # Podemos ter comentários na frente da linha de código
+
+# E também abaixo.
+
+"""
+Isso é uma DocString, o Python não ignora ele vai ler cada linha.
+e podemos ter miltiplas lihas
+
+A DocString server para documentar o código de forma clara e de facil manutentção
+"""
---
+++ Modulo 1/aula2.py
@@ -0,0 +1,24 @@
+print(12)
+"""
+A função print pode receber um ou mais argumentos,
+para adicionar mais argumentos precisamo colocar uma virgula
+e adicionar o proximo argumento
+"""
+
+print(35, 78)
+
+"""
+O print também possui argumentos não nomeados, note que na saida ele quebrou a linha 
+e na outra saida ele deu um espaço entre os números.
+"""
+
+"""Temos também os argumentos nomeados, onde podemos configurar enão deixar padrão"""
+
+print(12, 45, sep=" - ") # O argumento sep significa separador o nesse caso o espaço foi subistituido pelo "-"
+
+"""
+O argumento end=" " mexe no final dos argumentos, podemos adicionar qualquer coisa
+ou colocar uma quebra de linha
+"""
+
+print(12, 45, 1001, end='\n#')
